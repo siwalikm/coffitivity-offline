0eee2e81bce4762bcd639a244ec6ce418f9f1bb8
siwalikm <siwalikm@gmail.com>
2017-09-04 22:13:38 +0530

    🚀

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import { app, BrowserWindow } from 'electron';
+const path = require('path');
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+const createWindow = () => {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+  width: 300,
+  height: 500,
+  resizable: false,
+  titleBarStyle: 'hidden',
+  fullscreen: false,
+  movable: true,
+  icon: path.join(__dirname, '../assets/icon.ico')
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  // handling url on-clicks to open in default browser
+  var handleRedirect = (e, url) => {
+    if(url != mainWindow.webContents.getURL()) {
+      e.preventDefault()
+      require('electron').shell.openExternal(url)
+    }
+  }
+
+  mainWindow.webContents.on('will-navigate', handleRedirect)
+  mainWindow.webContents.on('new-window', handleRedirect)
+
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
---
+++ src/player.js
@@ -0,0 +1,50 @@
+let audio,$pause,$play;
+let triggerPage2 = (id) => {
+  if(audio != undefined) {
+      audio.pause();
+  }
+  audio = new Audio('../assets/'+id+'.mp3');
+  audio.play();
+  $pause.style.visibility = 'visible';
+  $play.style.visibility = 'hidden';
+  let elem = document.querySelector('#'+id);
+  let fetchedTitle = elem.querySelector('.musicOptBox__title').innerText;
+  document.querySelector('.musicOptBox.p2')
+          .querySelector('.musicOptBox__title')
+          .innerText=fetchedTitle;
+
+  let fetchedDesc = elem.querySelector('.musicOptBox__desc').innerText;
+  document.querySelector('.musicOptBox.p2')
+          .querySelector('.musicOptBox__desc')
+          .innerText=fetchedDesc;
+
+
+  document.querySelector('.p1').style.display = 'none';
+  document.querySelector('.p2').style.display = 'block';
+  document.querySelector('.musicOptBox.p2').style.display = 'block';
+}
+
+let triggerPage1 = () => {
+  document.querySelector('.p2').style.display = 'none';
+  document.querySelector('.p1').style.display = 'block';
+  // document.querySelector('.musicOptBox.p2').style.display = 'block';
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+  $pause = document.querySelector('.pausePlayBtn.icon-pause');
+  $play = document.querySelector('.pausePlayBtn.icon-play');
+  document.querySelector('.pausePlayContainer').addEventListener("click", () => {
+    if ($pause.style.visibility == 'visible') {
+      audio.pause();
+      $pause.style.visibility = 'hidden';
+      $play.style.visibility = 'visible';
+    } else {
+      audio.play();
+      $pause.style.visibility = 'visible';
+      $play.style.visibility = 'hidden';
+    }
+  });
+});
+
+// document.querySelector('.pausePlayBtn').onclick = () => {
+
+// };
